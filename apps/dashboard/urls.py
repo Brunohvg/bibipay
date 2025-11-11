2dbd426f04cb406233a6e46fdf875c40386a52a9
Bruno Vidal <brunovidal27.19@gmail.com>
2025-11-10 23:25:16 -0300

    refactor: remover referências ao BoxDashboardView e arquivo box.py

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -3,7 +3,7 @@ from .views import (
     dashboard_redirect_view,
     AdminDashboardView,
     SellerDashboardView,
-    # BoxDashboardView,
+
 )
 
 app_name = 'dashboard'
@@ -12,5 +12,5 @@ urlpatterns = [
     path('', dashboard_redirect_view, name='dashboard_redirect'),
     path('admin/', AdminDashboardView.as_view(), name='dashboard_admin'),
     path('sellers/', SellerDashboardView.as_view(), name='dashboard_sellers'),
-    # path('boxs/', BoxDashboardView.as_view(), name='dashboard_boxs'),
+
 ]
--- apps/dashboard/views/__init__.py
+++ apps/dashboard/views/__init__.py
@@ -1,5 +1,3 @@
 from .admin import AdminDashboardView
 from .seller import SellerDashboardView
 from .redirect import dashboard_redirect_view
-
-#from .box import BoxDashboardView
